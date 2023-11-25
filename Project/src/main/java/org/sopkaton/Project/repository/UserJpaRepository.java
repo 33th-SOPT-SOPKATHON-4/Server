8ab658ca5e88f6cea72265a9e43f52ddf1c2d2ae
이채은 <109871579+ChaeAg@users.noreply.github.com>
2023-11-26 02:35:22 +0900

    Revert "[FEAT] 로그인 및 회원가입 구현"

--- Project/src/main/java/org/sopkaton/Project/repository/UserJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package org.sopkaton.Project.repository;
-
-import org.sopkaton.Project.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserJpaRepository extends JpaRepository<User, String> {
-}
